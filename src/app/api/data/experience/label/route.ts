e97cffe5033e461785ca04dc9d2a0c139e762cc6
Literal Genie <>
2023-04-22 22:39:08 -0400

    Insert experience label

--- src/app/api/data/experience/label/route.ts
+++ src/app/api/data/experience/label/route.ts
@@ -1,4 +1,4 @@
-import * as ExperienceLabel from "@/app/models/indeed_post_label_experience"
+import * as ExperienceLabel from "@/models/indeed_post_label_experience"
 import { getDb } from "@/app/utils"
 import { NextResponse } from "next/server"
 
@@ -6,4 +6,11 @@ export async function GET() {
     const conn = getDb()
     const data = ExperienceLabel.getAll(conn)
     return NextResponse.json({ data })
+}
+
+export async function POST(request: Request) {
+    const conn = getDb()
+    const data = (await request.json()) as ExperienceLabel.PayloadCreate
+    const model = ExperienceLabel.insert(data, conn)
+    return NextResponse.json({ model })
 }
